efe3c7a6ab8282b5015e7dbe9cb4e5eeba115791
Eliot Mompeán <eliot.mompean@uah.es>
2024-10-28 17:42:09 +0100

    Cambios realizados en resultados01 para mejorar la visualización y reflejar resultados.

--- gitconfig.R
+++ gitconfig.R
@@ -5,3 +5,9 @@ use_git_config(user.name = "Eliot Mompeán", user.email = "eliot.mompean@uah.es"
 URL_DEL_REPOSITORIO <- "https://github.com/Eliot-MA/PR006-Cotiledones.git"
 system("git remote remove origin")
 system("git remote add origin https://github.com/Eliot-MA/PR006-Cotiledones.git")
+
+system("git init")
+
+system("git remote -v")
+
+system("git push -u origin master")  
