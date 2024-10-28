906f37890b21024906b229d87c08069467051d19
Eliot Mompeán <eliot.mompean@uah.es>
2024-10-28 16:05:19 +0100

    Intentando que esto funcione pero dando palos de ciego

---
+++ gitconfig.R
@@ -0,0 +1,7 @@
+# install.packages("usethis")
+library(usethis)
+use_git_config(user.name = "Eliot Mompeán", user.email = "eliot.mompean@uah.es")
+
+URL_DEL_REPOSITORIO <- "https://github.com/Eliot-MA/PR006-Cotiledones.git"
+system("git remote remove origin")
+system("git remote add origin https://github.com/Eliot-MA/PR006-Cotiledones.git")
